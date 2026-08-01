6bfd770b9f436ef352e2376dbfa82b76ee3ddc29
AnuragBojja <anuragbojja19@gmail.com>
2026-08-01 12:46:40 -0500

    over right parameter set to true

--- 00-vpc/ssm.tf
+++ 00-vpc/ssm.tf
@@ -2,6 +2,7 @@ resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project_name}/${var.env}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
+  overwrite = true
 }
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project_name}/${var.env}/public_subnet_ids"
